e36a88e9f4445482dd47886c164e91ef23a0d655
Ankur Varshney <76803416+Ankur-v-2004@users.noreply.github.com>
2024-10-10 15:49:12 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,9 @@ const javasc = 10;
 const pythonsc = 20;
 const answer = javasc + pythonsc;
 console.log(answer);
-
+//this is the comment 
+const an= 123;
+const ab = 234;
 function newfunc() {
   console.log("Hello")
 }
